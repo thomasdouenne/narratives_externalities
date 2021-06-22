92d73f972b8c9d311451a719206fa01a365fa273
thomas <thomas.douenne@sciencespo.fr>
2021-06-22 14:20:17 +0200

    Open file to prepare data from Qualtrics

---
+++ preparation.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+This script ...
+"""
+
+from __future__ import division
+
+
+import pandas as pd
+import numpy as np
+
+
+
+
+# Read data from Insee
+donnees_qualtrics = pd.read_csv(r'C:\Users\thoma\Documents\Github\narratives_externalities\preparation_survey\donnees_qualtrics_wip.csv', sep=',')
