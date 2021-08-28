7a27098ae3e5a1e83e8fc44a3979ddd20eba04ea
Thomas Douenne <t.r.g.r.douenne@uva.nl>
2021-08-28 11:43:21 +0200

    Add tags to airtravel

--- analysis_open_questions_pilot.py
+++ analysis_open_questions_pilot.py
@@ -21,30 +21,30 @@ data_pilot['airtravel'] = data_pilot['full'].query('topic_airtravel == 1').copy(
 data_pilot['vehicle'] = data_pilot['full'].query('topic_vehicle == 1').copy()
 data_pilot['vaccine'] = data_pilot['full'].query('topic_vaccine == 1').copy()
 
-### End of survey box
+##### End of survey box
 answers_open_end_box = data_pilot['full']['open_box_end'].dropna()
 answers_open_end_box.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\open_end_box.csv',
                             sep=';', encoding='utf-8-sig', index=True)
 
 ### Meat
 answers_meat_why_against_open = data_pilot['meat']['meat_why_against_open'].dropna()
-answers_meat_why_against_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\meat_why_against.csv',
+answers_meat_why_against_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Meat\meat_why_against.csv',
                             sep=';', encoding='utf-8-sig', index=True)
 
 answers_meat_why_favorable_open = data_pilot['meat']['meat_why_favorable_open'].dropna()
-answers_meat_why_favorable_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\meat_why_favorable.csv',
+answers_meat_why_favorable_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Meat\meat_why_favorable.csv',
                             sep=';', encoding='utf-8-sig', index=True)
 
 answers_meat_why_neutral_open = data_pilot['meat']['meat_why_neutral_open'].dropna()
-answers_meat_why_neutral_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\meat_why_neutral.csv',
+answers_meat_why_neutral_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Meat\meat_why_neutral.csv',
                             sep=';', encoding='utf-8-sig', index=True)
 
 answers_meat_landscape_argument_open = data_pilot['meat']['meat_landscape_argument_open'].dropna()
-answers_meat_landscape_argument_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\meat_landscape_argument.csv',
+answers_meat_landscape_argument_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Meat\meat_landscape_argument.csv',
                             sep=';', encoding='utf-8-sig', index=True)
 
 answers_meat_carbon_argument_open = data_pilot['meat']['meat_carbon_argument_open'].dropna()
-answers_meat_carbon_argument_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\meat_carbon_argument.csv',
+answers_meat_carbon_argument_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Meat\meat_carbon_argument.csv',
                             sep=';', encoding='utf-8-sig', index=True)
 
 dict_tags_meat = {}
@@ -173,5 +173,140 @@ for element in dict_tags_meat['carbon_footprint_argument'].keys():
     data_pilot['meat'].loc[dict_tags_meat['carbon_footprint_argument'][element],'tags_answer_carbon_footprint_argument'] = \
         element + ';' + data_pilot['meat'].loc[dict_tags_meat['carbon_footprint_argument'][element],'tags_answer_carbon_footprint_argument']
 
-# TODO: count the share of answered
 
+##### Air travel
+answers_airtravel_why_against_open = data_pilot['airtravel']['airtravel_why_against_open'].dropna()
+answers_airtravel_why_against_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Airtravel\airtravel_why_against.csv',
+                            sep=';', encoding='utf-8-sig', index=True)
+
+answers_airtravel_why_favorable_open = data_pilot['airtravel']['airtravel_why_favorable_open'].dropna()
+answers_airtravel_why_favorable_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Airtravel\airtravel_why_favorable.csv',
+                            sep=';', encoding='utf-8-sig', index=True)
+
+answers_airtravel_why_neutral_open = data_pilot['airtravel']['airtravel_why_neutral_open'].dropna()
+answers_airtravel_why_neutral_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Airtravel\airtravel_why_neutral.csv',
+                            sep=';', encoding='utf-8-sig', index=True)
+
+answers_airtravel_open_mindedness_argument_open = data_pilot['airtravel']['airtravel_open_mindedness_argument_open'].dropna()
+answers_airtravel_open_mindedness_argument_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Airtravel\airtravel_open_mindedness_argument.csv',
+                            sep=';', encoding='utf-8-sig', index=True)
+
+answers_airtravel_carbon_argument_open = data_pilot['airtravel']['airtravel_carbon_argument_open'].dropna()
+answers_airtravel_carbon_argument_open.to_csv(r'C:\Users\TDOUENN\Documents\Projects\Narratives\Data\Text_open_questions\Airtravel\airtravel_carbon_argument.csv',
+                            sep=';', encoding='utf-8-sig', index=True)
+
+dict_tags_airtravel = {}
+dict_tags_airtravel['why_against'] = {
+    'Pollution': [12,16,25,26,41,86,124,142,147,165,171,178,184,214,215,
+                  270,291,312,318,361,367,378,390,392,425,431,484,503,508,
+                  527,544,653,715,903,1227,3285,3762],
+    'Virus_spreading': [12,297,322],
+    'Noise': [378],
+    'Price_expensive': [104,343,419,433,461,653,824],
+    'Better_alternatives_exist': [104,178,247,392,425,670,715,3285],
+    'Not_always_convenient': [247,419,460],
+    'Risky': [],
+    'Opposite_argument': [288,318,408,546,698,965],
+    'Dont_know': [3775],
+    'Unclear_undetermined': [126,252,322,386,442,3224,3775],
+    }
+
+dict_tags_airtravel['why_favorable'] = {
+    'Discover_the_world': [44,66,108,153,200,225,229,374,389,420,971,1278],
+    'To_go_everywhere': [51,63,66,225,229,335,374,389,420,971,1278],
+    'Quicker': [51,63,79,107,114,119,136,175,233,235,254,279,783,971,1101],
+    'Convenient': [79,107,114,138,971,1101,1278],
+    'Economy': [98,420,481],
+    'Evasion': [108,430,438,577,3239],
+    'Safer': [114,783],
+    'Price_cheap': [119,138],
+    'Pollutes_less': [198,235,326,358],
+    'Less_road_traffic': [],
+    'Politicians_wealthy_do_it': [349],
+    'Opposite_argument': [229,301,335,971],
+    'Dont_know': [260],
+    'Unclear_undetermined': [160,198,258,358,737,1200],
+    }
+
+dict_tags_airtravel['why_neutral'] = {
+    'Pollution': [22,83,89,287,310,385,509,636,675,732,856,896,939,2706,3231,3235],
+    'Virus_spreading': [201],
+    'Noise': [],
+    'Price_expensive': [104,343,419,433,461,653,824]+[983,1175],
+    'Better_alternatives_exist': [],
+    'Not_always_convenient': [287,332,432],
+    'Risky': [74],
+    'Discover_the_world': [69,597],
+    'To_go_everywhere': [53,69,287,636,732,896],
+    'Quicker': [74,287,385,432,636,697,896,983,3231],
+    'Convenient': [332,597,732],
+    'Economy': [636,843],
+    'Evasion': [597],
+    'Safer': [],
+    'Pollutes_less': [22],
+    'Less_road_traffic': [675,939,1175],
+    'Politicians_wealthy_do_it': [407],
+    'Dont_know': [192,224,231,346,364,3282],
+    'Unclear_undetermined': [3238],
+    }
+
+dict_tags_airtravel['open_mindedness_argument'] = {
+    'Agree': [16,22,25,26,41,69,86,89,94,104,124,142,147,165,171,184,185,201,
+              214,215,224,231,247,252,270,288,291,310,318,332,343,361,364,367,
+              385,386,390,392,407,408,419,425,432,433,452,460,461,471,484,503,
+              508,509,527,546,636,653,670,675,697,698,715,732,824,843,856,896,
+              903,939,965,983,1175,1227,3224,3231,3235,3282,3285,3775],
+    'Not_true': [53,74,83,178,192,312,322,346,378],
+    'It_depends': [12,41,392,460,670,843,2706,3231,3235],
+    'Uncertain': [322,346,732],
+    'Other_modes_also_do_it': [12,53,178,231,378,431,843,3762],
+    'Can_travel_differently': [83,270,312,670],
+    'Unrelated_opposite_argument': [86,104,201,322,432,544,597],
+    'Dont_know': [],
+    'Unclear_undetermined': [94,126,442,3238],
+    }
+
+dict_tags_airtravel['carbon_footprint_argument'] = {
+    'Agree': [63,66,98,107,108,119,136,153,160,254,279,301,335,389,420,577,783,971],
+    'Not_true': [51,138,175,198,200,233,235,358,374,3239],
+    'It_depends': [51,79,136,160,326,737,783],
+    'Uncertain': [114,233,430,438],
+    'Other_things_pollute_more': [44,258,349],
+    'Unrelated_opposite_argument': [349,420,1278],
+    'Dont_know': [225,229,260,1101],
+    'Unclear_undetermined': [79,258,1200],
+    }
+
+
+data_pilot['airtravel']['tags_answer_why_against'] = ''
+for element in dict_tags_airtravel['why_against'].keys():
+    data_pilot['airtravel'].loc[dict_tags_airtravel['why_against'][element],'tags_answer_why_against'] = \
+        element + ';' + data_pilot['airtravel'].loc[dict_tags_airtravel['why_against'][element],'tags_answer_why_against']
+
+data_pilot['airtravel']['tags_answer_why_neutral'] = ''
+for element in dict_tags_airtravel['why_neutral'].keys():
+    data_pilot['airtravel'].loc[dict_tags_airtravel['why_neutral'][element],'tags_answer_why_neutral'] = \
+        element + ';' + data_pilot['airtravel'].loc[dict_tags_airtravel['why_neutral'][element],'tags_answer_why_neutral']
+
+data_pilot['airtravel']['tags_answer_why_favorable'] = ''
+for element in dict_tags_airtravel['why_favorable'].keys():
+    data_pilot['airtravel'].loc[dict_tags_airtravel['why_favorable'][element],'tags_answer_why_favorable'] = \
+        element + ';' + data_pilot['airtravel'].loc[dict_tags_airtravel['why_favorable'][element],'tags_answer_why_favorable']
+
+data_pilot['airtravel']['tags_answer_open_mindedness_argument'] = ''
+for element in dict_tags_airtravel['open_mindedness_argument'].keys():
+    data_pilot['airtravel'].loc[dict_tags_airtravel['open_mindedness_argument'][element],'tags_answer_open_mindedness_argument'] = \
+        element + ';' + data_pilot['airtravel'].loc[dict_tags_airtravel['open_mindedness_argument'][element],'tags_answer_open_mindedness_argument']
+
+data_pilot['airtravel']['tags_answer_carbon_footprint_argument'] = ''
+for element in dict_tags_airtravel['carbon_footprint_argument'].keys():
+    data_pilot['airtravel'].loc[dict_tags_airtravel['carbon_footprint_argument'][element],'tags_answer_carbon_footprint_argument'] = \
+        element + ';' + data_pilot['airtravel'].loc[dict_tags_airtravel['carbon_footprint_argument'][element],'tags_answer_carbon_footprint_argument']
+
+
+
+
+
+# TODO: count the share of answered
+# Do some stats over tags
+# Display most common words
